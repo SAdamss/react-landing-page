50dfa1a3e1f1c9a394d1362f5511e4b979cb22ec
Shannon <shannonadams083@gmail.com>
2022-07-23 14:26:11 +0100

    basic navbar info

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+export const Navbar = () => {
+  return (
+    <div>
+    <ul>
+    <li>Home</li>
+    <li>Work</li>
+    <li>Socials</li>
+    <li>Contact</li>
+    </ul>
+    </div>
+  )
+}
