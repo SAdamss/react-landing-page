4dd6bbe02580b05caee9ada771b84e4ec51d12f7
Shannon <shannonadams083@gmail.com>
2022-07-27 14:16:01 +0100

    LF replacement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import React, { useState } from 'react';
+import {
+    FaBars,
+    FaTimes,
+    FaInstagramSquare,
+    FaTwitterSquare,
+    FaLinkedInSquare,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
+
 export const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
   return (
     <div>
     <ul>
